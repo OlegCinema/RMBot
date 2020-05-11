51e712ad1b860a30f6b265c268739019d88827da
Олег Кинема <36475439+OlegCinema@users.noreply.github.com>
2020-05-11 13:35:32 +1000

    correct

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ module.exports = {
 			.setTitle('ReactionManagerBot')
 			.setURL('https://github.com/OlegCinema/RMBot')
 			.setAuthor('OlegCinema', 'https://i.imgur.com/Q6FXBYR.jpg', 'https://github.com/OlegCinema')
-			.setDescription('Discord bot, written in JS, is using for adding or deleting roles in guilds.')
+			.setDescription('Discord bot, written in JS, is used for adding or deleting roles in guilds.')
 			.setThumbnail('https://i.imgur.com/53H5Ot8.png')
 			.setTimestamp()
 			.setFooter('You can contact by email: olegcinemaw@mail.ru');
